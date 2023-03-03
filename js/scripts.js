7c2b4e4d2525ff17372cbfd218828d09c418a036
OBo1307 <och.bogdan@gmail.com>
2023-03-03 12:27:15 +0000

    added pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-alert("Hello world");
-
-let favoriteFood = "Pizza";
-document.write(favoriteFood);
+let pokemonList = [
+    {name: "charmeleon", weight: 19, type: ["fire","rock"]},
+    {name: "ponyta", weight: 30, type: ["ice","steel"]},
+    {name: "squirtle", weight: 9, type: ["water","grass"]}
+    {name: "alakazam", weight: 48, type: ["ghost","dragon"]}
+];
