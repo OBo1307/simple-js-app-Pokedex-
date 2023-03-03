eca5f4be20ba795d10307450108efb7cbebeabdb
OBo1307 <och.bogdan@gmail.com>
2023-03-03 16:24:43 +0000

    added loop for pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,21 @@
 let pokemonList = [
-    {name: "charmeleon", weight: 19, type: ["fire","rock"]},
-    {name: "ponyta", weight: 30, type: ["ice","steel"]},
-    {name: "squirtle", weight: 9, type: ["water","grass"]}
-    {name: "alakazam", weight: 48, type: ["ghost","dragon"]}
+    {name: "Charmeleon", weight: 19, type: ["fire","rock"]},
+    {name: "Ponyta", weight: 30, type: ["ice","steel"]},
+    {name: "Squirtle", weight: 9, type: ["water","grass"]},
+    {name: "Alakazam", weight: 48, type: ["ghost","dragon"]}
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write(
+      pokemonList[i].name +
+        " " +
+        "(Weight:" +
+        pokemonList[i].weight +
+        ")" +
+        " " +
+        "(Type:" +
+        pokemonList[i].type +
+        ")" +
+        "<br>"
+    );
+  }
