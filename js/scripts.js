bb0b69cbe589e34d522029f617640e30242632d8
OBo1307 <och.bogdan@gmail.com>
2023-03-10 11:21:18 +0000

    updated to forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -5,32 +5,30 @@ let pokemonList = [
     {name: "Alakazam", weight: 48, type: ["ghost","dragon"]}
 ];
 
-for (let i = 0; i < pokemonList.length; i++) {
+// for loop
+
+/*for (let i = 0; i < pokemonList.length; i++) {
     if (pokemonList[i].weight >= 40) {
         document.write(
-            pokemonList[i].name +
-              " " +
-              "(Weight: " +
-              pokemonList[i].weight +
-              ")" +
-              " " +
-              "(Type: " +
-              pokemonList[i].type +
-              ")" + " - Wow, that's big!" + 
-              "<br>"
+            `${pokemonList[i].name} (Weight: ${pokemonList[i].weight}) (Type: ${pokemonList[i].type}) - Wow, that's big!<br>`
           );
     } else {
         document.write(
-            pokemonList[i].name +
-              " " +
-              "(Weight: " +
-              pokemonList[i].weight +
-              ")" +
-              " " +
-              "(Type: " +
-              pokemonList[i].type +
-              ")" +
-              "<br>"
+            `${pokemonList[i].name} (Weight: ${pokemonList[i].weight}) (Type: ${pokemonList[i].type})<br>`
           );
     }
-  }
+  } */
+
+// forEach loop
+
+pokemonList.forEach(function(pokemon) {
+    if (pokemon.weight >= 40) {
+        document.write(
+            `${pokemon.name} (Weight: ${pokemon.weight}) (Type: ${pokemon.type}) - Wow, that's big!<br>`
+          );
+    } else {
+        document.write(
+            `${pokemon.name} (Weight: ${pokemon.weight}) (Type: ${pokemon.type})<br>`
+          );
+    }  
+});
